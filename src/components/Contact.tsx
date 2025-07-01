4591cd8d63088cecc575e736c877af35cb8c7ce8
minhhq <homieg198204@gmail.com>
2025-07-01 09:26:58 +0700

    fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -75,7 +75,7 @@ export const Contact = () => {
     }
 
     try {
-      const res = await fetch("https://mysql-production-f6cb.up.railway.app/contact", {
+      const res = await fetch("/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
